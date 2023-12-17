2fc67487eb0160768eae21cc1ab8da116afa1bd1
Zack H <zphixon@gmail.com>
2023-12-17 16:02:16 -0500

    day 12 part 2

--- src/day12.rs
+++ src/day12.rs
@@ -1,4 +1,5 @@
 use std::{
+    collections::HashMap,
     fmt::{Debug, Display},
     str::FromStr,
 };
@@ -25,12 +26,14 @@ pub fn run(example: bool) {
     );
 }
 
-#[derive(Clone, Copy, PartialEq, Eq)]
+#[derive(Clone, Copy, PartialEq, Eq, Hash)]
 enum State {
     Working,
     Broken,
     Unknown,
 }
+
+unsafe impl Send for State {}
 
 impl State {
     fn as_str(&self) -> &'static str {
@@ -79,6 +82,23 @@ struct Record {
     runs: Vec<usize>,
 }
 
+impl Record {
+    fn unfold(&self) -> Record {
+        let mut states = self.states.clone();
+        for _ in 2..=5 {
+            states.push(State::Unknown);
+            states.extend(self.states.iter().copied());
+        }
+
+        let mut runs = self.runs.clone();
+        for _ in 2..=5 {
+            runs.extend(self.runs.iter().copied());
+        }
+
+        Record { states, runs }
+    }
+}
+
 impl Display for Record {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         for state in self.states.iter() {
@@ -116,48 +136,58 @@ fn parse(data: &str) -> Vec<Record> {
         .collect()
 }
 
-fn num_arrangements_rec(mut states: &[State], runs: &[usize], indent_n: usize) -> usize {
+fn num_arrangements_rec<'data>(
+    memo: &mut HashMap<(&'data [State], &'data [usize]), usize>,
+    states: &'data [State],
+    runs: &'data [usize],
+    indent_n: usize,
+) -> usize {
+    if let Some(&memoed) = memo.get(&(states, runs)) {
+        return memoed;
+    }
+
     let indent = "|  ".repeat(indent_n);
 
     tracing::trace!("{indent}states={states:?} runs={runs:?}");
 
+    let mut trimmed = states;
     // remove leading and trailing .s
-    while !states.is_empty() && states[0] == State::Working {
-        states = &states[1..];
+    while !trimmed.is_empty() && trimmed[0] == State::Working {
+        trimmed = &trimmed[1..];
     }
-    while !states.is_empty() && states[states.len() - 1] == State::Working {
-        states = &states[..states.len() - 1];
+    while !trimmed.is_empty() && trimmed[trimmed.len() - 1] == State::Working {
+        trimmed = &trimmed[..trimmed.len() - 1];
     }
 
-    if states.is_empty() && runs.is_empty() {
+    if trimmed.is_empty() && runs.is_empty() {
         // it looks like this never happens?
         tracing::trace!("{indent}nice 1");
         return 1;
     }
 
-    if states.iter().all(State::maybe_working) && runs.is_empty() {
+    if trimmed.iter().all(State::maybe_working) && runs.is_empty() {
         // everything left is ? or . and no more runs left
         tracing::trace!("{indent}nice 2");
         return 1;
     }
 
-    if states.is_empty() || runs.is_empty() {
+    if trimmed.is_empty() || runs.is_empty() {
         tracing::trace!("{indent}empty");
         return 0;
     }
 
     let run = runs[0];
-    tracing::trace!("{indent}run={run} trimmed={states:?}");
+    tracing::trace!("{indent}run={run} trimmed={trimmed:?}");
 
-    if run > states.len() {
+    if run > trimmed.len() {
         tracing::trace!("{indent}not enough states left");
         return 0;
     }
 
     if runs.len() == 1
-        && run <= states.len()
-        && states[0..run].iter().all(State::maybe_broken)
-        && states[run..].iter().all(|&state| state == State::Working)
+        && run <= trimmed.len()
+        && trimmed[0..run].iter().all(State::maybe_broken)
+        && trimmed[run..].iter().all(|&state| state == State::Working)
     {
         // everything within the run is ? or # and everything beyond is .
         // (should this also count #, idk?)
@@ -166,13 +196,13 @@ fn num_arrangements_rec(mut states: &[State], runs: &[usize], indent_n: usize) -
     }
 
     let mut left = None;
-    let mut right = states.get(run).copied();
+    let mut right = trimmed.get(run).copied();
     let mut sum = 0;
-    for (window_num, window) in states.windows(run).enumerate() {
+    for (window_num, window) in trimmed.windows(run).enumerate() {
         tracing::trace!(
             "{indent}run={run} left={left:?} window={window:?} right={right:?} window_num={window_num}"
         );
-        
+
         let must_go_here = window[0] == State::Broken;
         if must_go_here {
             tracing::trace!("{indent}must go here");
@@ -186,7 +216,7 @@ fn num_arrangements_rec(mut states: &[State], runs: &[usize], indent_n: usize) -
             && window[..run].iter().all(State::maybe_broken)
             && right != Some(State::Broken)
         {
-            if window_num + run + 1 >= states.len() {
+            if window_num + run + 1 >= trimmed.len() {
                 if runs.len() == 1 {
                     // right will be None after this, and we cannot recur, but
                     // it fits here
@@ -198,13 +228,17 @@ fn num_arrangements_rec(mut states: &[State], runs: &[usize], indent_n: usize) -
             } else {
                 // check states after R (....) for the next run
                 tracing::trace!("{indent}fits");
-                sum +=
-                    num_arrangements_rec(&states[window_num + run + 1..], &runs[1..], indent_n + 1);
+                sum += num_arrangements_rec(
+                    memo,
+                    &trimmed[window_num + run + 1..],
+                    &runs[1..],
+                    indent_n + 1,
+                );
             }
         }
 
         left = Some(window[0]);
-        right = states.get(window_num + run + 1).copied();
+        right = trimmed.get(window_num + run + 1).copied();
 
         if must_go_here {
             // the first spring in this window is broken, the run cannot appear
@@ -219,11 +253,13 @@ fn num_arrangements_rec(mut states: &[State], runs: &[usize], indent_n: usize) -
         }
     }
 
+    memo.insert((states, runs), sum);
     sum
 }
 
 fn num_arrangements(states: &[State], runs: &[usize]) -> usize {
-    num_arrangements_rec(states, runs, 0)
+    let mut memo = HashMap::new();
+    num_arrangements_rec(&mut memo, states, runs, 0)
 }
 
 fn part1(data: &str) -> usize {
@@ -239,7 +275,13 @@ fn part1(data: &str) -> usize {
 }
 
 fn part2(data: &str) -> usize {
-    0
+    parse(data)
+        .into_iter()
+        .map(|record| {
+            let Record { states, runs } = record.unfold();
+            num_arrangements(&states, &runs)
+        })
+        .sum()
 }
 
 #[test]
@@ -249,5 +291,5 @@ fn test1() {
 
 #[test]
 fn test2() {
-    assert_eq!(part2(include_str!("../data/day12.1.txt")), 512240933238);
+    assert_eq!(part2(include_str!("../data/day12.1.txt")), 850504257483930);
 }

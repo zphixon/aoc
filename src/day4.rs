77bc12fd39dd00b11bb5d86294f02c3b9b4137d3
Zack H <zphixon@gmail.com>
2024-12-04 12:10:41 -0500

    day4

---
+++ src/day4.rs
@@ -0,0 +1,168 @@
+use crate::util;
+
+pub fn run(example: bool) {
+    let data = if example {
+        include_str!("../data/example/day4.1.txt")
+    } else {
+        include_str!("../data/day4.1.txt")
+    };
+
+    tracing::info!("day 4 part 1{}", if example { " example" } else { "" });
+    tracing::info!(
+        "day 4 part 1{} result: {}",
+        if example { " example" } else { "" },
+        part1(data),
+    );
+    tracing::info!("day 4 part 2{}", if example { " example" } else { "" });
+    tracing::info!(
+        "day 4 part 2{} result: {}",
+        if example { " example" } else { "" },
+        part2(data), // same data
+    );
+}
+
+fn get_plane(data: &str) -> Vec<Vec<char>> {
+    let mut plane = Vec::new();
+
+    for row in data.lines() {
+        plane.push(Vec::new());
+        for col in row.chars() {
+            plane.last_mut().unwrap().push(col);
+        }
+    }
+
+    plane
+}
+
+fn part1(data: &str) -> usize {
+    let mut got = Vec::<[(char, usize, usize, util::Direction); 4]>::new();
+
+    let plane = get_plane(data);
+
+    for (xrow, r) in plane.iter().enumerate() {
+        for (xcol, &x) in r.iter().enumerate() {
+            if x == 'X' {
+                tracing::trace!("got an X");
+                for (&m, mrow, mcol, mdir) in
+                    util::surrounding(plane.as_slice(), xrow, xcol).filter(|(&m, _, _, _)| m == 'M')
+                {
+                    tracing::trace!("got an M {:?}", mdir);
+                    'next_a: for (&a, arow, acol, adir) in
+                        util::surrounding(plane.as_slice(), mrow, mcol)
+                            .filter(|(&a, _, _, _)| a == 'A')
+                    {
+                        tracing::trace!("got an A {:?}", adir);
+                        if adir != mdir {
+                            tracing::trace!("wrong direction");
+                            continue 'next_a;
+                        }
+
+                        'next_s: for (&s, srow, scol, sdir) in
+                            util::surrounding(plane.as_slice(), arow, acol)
+                                .filter(|(&s, _, _, _)| s == 'S')
+                        {
+                            tracing::trace!("got an S");
+                            if sdir != adir {
+                                tracing::trace!("wrong direction");
+                                continue 'next_s;
+                            }
+
+                            tracing::debug!(
+                                "gotcha! X={},{} M={},{} A={},{} S={},{}",
+                                xrow,
+                                xcol,
+                                mrow,
+                                mcol,
+                                arow,
+                                acol,
+                                srow,
+                                scol
+                            );
+                            got.push([
+                                (x, xrow, xcol, util::Direction::E),
+                                (m, mrow, mcol, mdir),
+                                (a, arow, acol, adir),
+                                (s, srow, scol, sdir),
+                            ]);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    let mut vis = vec![vec!['.'; plane[0].len()]; plane.len()];
+    for [(x, xrow, xcol, _), (m, mrow, mcol, _), (a, arow, acol, _), (s, srow, scol, _)] in
+        got.iter()
+    {
+        vis[*xrow][*xcol] = *x;
+        vis[*mrow][*mcol] = *m;
+        vis[*arow][*acol] = *a;
+        vis[*srow][*scol] = *s;
+    }
+
+    for row in vis {
+        tracing::debug!("{:?}", row);
+    }
+
+    got.len()
+}
+
+fn part2(data: &str) -> usize {
+    let mut got = Vec::new();
+
+    let plane = get_plane(data);
+    for (row, r) in plane.iter().enumerate().take(plane.len() - 1).skip(1) {
+        'next_a: for (col, &c) in r.iter().enumerate().take(r.len() - 1).skip(1) {
+            if c != 'A' {
+                continue 'next_a;
+            }
+
+            let around = util::surrounding(plane.as_slice(), row, col).collect::<Vec<_>>();
+            match around.as_slice() {
+                // M M      M S      S S      S M
+                //  A        A        A        A
+                // S S      M S      M M      S M
+                [(c1 @ &'M', row1, col1, _), _, (c2 @ &'M', row2, col2, _), _, _, (c3 @ &'S', row3, col3, _), _, (c4 @ &'S', row4, col4, _)]
+                | [(c1 @ &'M', row1, col1, _), _, (c2 @ &'S', row2, col2, _), _, _, (c3 @ &'M', row3, col3, _), _, (c4 @ &'S', row4, col4, _)]
+                | [(c1 @ &'S', row1, col1, _), _, (c2 @ &'S', row2, col2, _), _, _, (c3 @ &'M', row3, col3, _), _, (c4 @ &'M', row4, col4, _)]
+                | [(c1 @ &'S', row1, col1, _), _, (c2 @ &'M', row2, col2, _), _, _, (c3 @ &'S', row3, col3, _), _, (c4 @ &'M', row4, col4, _)] =>
+                {
+                    got.push([
+                        (**c1, *row1, *col1),
+                        (**c2, *row2, *col2),
+                        (**c3, *row3, *col3),
+                        (**c4, *row4, *col4),
+                    ]);
+                }
+
+                _ => {}
+            }
+        }
+    }
+
+    let mut vis = vec![vec!['.'; plane[0].len()]; plane.len()];
+    for [(x, xrow, xcol), (m, mrow, mcol), (a, arow, acol), (s, srow, scol)] in got.iter() {
+        vis[*xrow][*xcol] = *x;
+        vis[*mrow][*mcol] = *m;
+        vis[*arow][*acol] = *a;
+        vis[*srow][*scol] = *s;
+        vis[xrow + 1][xcol + 1] = 'A';
+    }
+
+    for row in vis {
+        tracing::debug!("{:?}", row);
+    }
+
+    got.len()
+}
+
+#[test]
+fn test1() {
+    assert_eq!(part1(include_str!("../data/day4.1.txt")), 2176849);
+}
+
+#[test]
+fn test2() {
+    assert_eq!(part2(include_str!("../data/day4.1.txt")), 23384288);
+}
--- src/util.rs
+++ src/util.rs
@@ -1,6 +1,6 @@
-use std::{collections::HashMap, hash::Hash};
+use std::{collections::HashMap, fmt::Debug, hash::Hash};
 
-pub fn frequency<K: Eq + Hash>(iter: impl Iterator<Item=K>) -> HashMap<K, u64> {
+pub fn frequency<K: Eq + Hash>(iter: impl Iterator<Item = K>) -> HashMap<K, u64> {
     let mut counts = HashMap::new();
 
     for item in iter {
@@ -8,4 +8,105 @@ pub fn frequency<K: Eq + Hash>(iter: impl Iterator<Item=K>) -> HashMap<K, u64> {
     }
 
     counts
+}
+
+#[rustfmt::skip]
+#[derive(Clone, Copy, PartialEq, Eq, Debug)]
+pub enum Direction {
+    NW, N, NE,
+     W,     E,
+    SW, S, SE,
+}
+
+impl Direction {
+    pub fn to_offset(&self) -> (isize, isize) {
+        use Direction::*;
+        match self {
+            NW => (-1, -1),
+            N => (-1, 0),
+            NE => (-1, 1),
+            W => (0, -1),
+            E => (0, 1),
+            SW => (1, -1),
+            S => (1, 0),
+            SE => (1, 1),
+        }
+    }
+
+    pub fn apply<'a, T>(
+        &self,
+        plane: &'a [Vec<T>],
+        row: usize,
+        col: usize,
+    ) -> Option<(usize, usize)> {
+        let (row, col) = (row as isize, col as isize);
+        let (row_off, col_off) = self.to_offset();
+
+        let off_row = row + row_off;
+        let off_col = col + col_off;
+
+        if (0 <= off_row && off_row < plane.len() as isize)
+            && (0 <= off_col && off_col < plane[off_row as usize].len() as isize)
+            && !(row_off == 0 && col_off == 0)
+        {
+            Some((off_row as usize, off_col as usize))
+        } else {
+            None
+        }
+    }
+}
+
+pub type SurroundingItem<'a, T> = (&'a T, usize, usize, Direction);
+
+pub struct Surrounding<'a, T> {
+    plane: &'a [Vec<T>],
+    dir: usize,
+    row: usize,
+    col: usize,
+}
+
+impl<'a, T: Debug> Iterator for Surrounding<'a, T> {
+    type Item = SurroundingItem<'a, T>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        use Direction::*;
+        let dirs = [NW, N, NE, W, E, SW, S, SE];
+        if self.dir >= dirs.len() {
+            tracing::trace!("no more dirs surrounding {},{}", self.row, self.col);
+            return None;
+        }
+
+        let dir = dirs[self.dir];
+        self.dir += 1;
+
+        if let Some((off_row, off_col)) = dir.apply(&self.plane, self.row, self.col) {
+            tracing::trace!(
+                "{:?} {},{} is {:?} of {},{}",
+                self.plane[off_row][off_col],
+                off_row,
+                off_col,
+                dir,
+                self.row,
+                self.col
+            );
+            Some((&self.plane[off_row][off_col], off_row, off_col, dir))
+        } else {
+            tracing::trace!("nothing is {:?} of {},{}", dir, self.row, self.col);
+            self.next()
+        }
+    }
+}
+
+pub fn surrounding<'a, T: Debug>(
+    plane: &'a [Vec<T>],
+    row: usize,
+    col: usize,
+) -> impl Iterator<Item = SurroundingItem<'a, T>> {
+    tracing::trace!("look for items surrounding {:?} {},{}", plane[row][col], row, col);
+    Surrounding {
+        plane,
+        row,
+        col,
+        dir: 0,
+    }
 }

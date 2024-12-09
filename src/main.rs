50f065b0e76ba893ec5e83df75dcab91c0ce4d1c
Zack H <zphixon@gmail.com>
2024-12-09 12:51:07 -0500

    new helper ding

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,55 @@
+use std::io::prelude::*;
+
 pub mod util;
 
+const TEMPLATE: &str = r#"
+pub fn run(example: bool) {
+    let data = if example {
+        include_str!("../data/example/day{day}.1.txt")
+    } else {
+        include_str!("../data/day{day}.1.txt")
+    };
+
+    tracing::info!("day {day} part 1{}", if example { " example" } else { "" });
+    tracing::info!(
+        "day {day} part 1{} result: {}",
+        if example { " example" } else { "" },
+        part1(data),
+    );
+
+    if example {
+        tracing::warn!("used example data");
+    }
+
+    tracing::info!("day {day} part 2{}", if example { " example" } else { "" });
+    tracing::info!(
+        "day {day} part 2{} result: {}",
+        if example { " example" } else { "" },
+        part2(data), // same data
+    );
+}
+
+fn part1(data: &str) -> u64 {
+    0
+}
+
+fn part2(data: &str) -> u64 {
+    0
+}
+
+#[test]
+fn test1() {
+    assert_eq!(part1(include_str!("../data/day{day}.1.txt")), 0);
+}
+
+#[test]
+fn test2() {
+    assert_eq!(part2(include_str!("../data/day{day}.1.txt")), 0);
+}
+"#;
+
 macro_rules! main {
-    ($($day_nums:tt),*) => { paste::paste! {
+    ($($day_nums:tt),* $(,)?) => { paste::paste! {
         $(mod [< day $day_nums >];)*
 
         fn main() {
@@ -9,8 +57,32 @@ macro_rules! main {
 
             let args = std::env::args().collect::<Vec<_>>();
             let example_arg = args.iter().any(|arg| arg == "--example");
+            let new_day_arg = args.iter().any(|arg| arg == "--new-day");
+
             let day_args = args.iter().filter_map(|arg| arg.parse::<u8>().ok()).collect::<Vec<_>>();
             let day_nums = vec![$(stringify!($day_nums).parse::<u8>().unwrap()),*];
+
+            if new_day_arg {
+                for day in day_args {
+                    std::fs::write(format!("data/example/day{}.1.txt", day), "").unwrap();
+                    std::fs::write(format!("src/day{}.rs", day), TEMPLATE.replace("{day}", &format!("{}", day))).unwrap();
+                    let mut main_rs = std::fs::OpenOptions::new()
+                        .read(true)
+                        .write(true)
+                        .create(false)
+                        .open("src/main.rs")
+                        .unwrap();
+                    let mut contents = String::new();
+                    main_rs.read_to_string(&mut contents).unwrap();
+                    main_rs.set_len(0).unwrap();
+                    main_rs.seek(std::io::SeekFrom::Start(0)).unwrap();
+                    let lol = "// new ";
+                    let xd = "days here";
+                    let lolxd = format!("{}{}", lol, xd);
+                    main_rs.write_all(contents.replace(&lolxd, &format!("{},\n    {}", day, lolxd)).as_bytes()).unwrap();
+                }
+                return;
+            }
 
             let mut not_a_day = false;
             for day_arg in day_args.iter() {
@@ -48,4 +120,7 @@ macro_rules! main {
     } };
 }
 
-main!(1, 2, 3, 4, 5, 6, 7, 8);
+main!(
+    1, 2, 3, 4, 5, 6, 7, 8,
+    // new days here
+);
